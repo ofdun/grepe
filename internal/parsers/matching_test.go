a08fcadfdce7a829fb88800ecd4f7027bed065c6
Elisey Kalashnikov <elisey0380231@yandex.ru>
2024-12-30 15:05:08 +0300

    refactoring: added some tests and fixed unparam

--- internal/parsers/matching_test.go
+++ internal/parsers/matching_test.go
@@ -7,12 +7,12 @@ import (
 	"testing"
 )
 
-func init3DSlice(x, y, z int) [][][]int {
+func init3DSlice(x, y, _z int) [][][]int {
 	slice := make([][][]int, x)
 	for i := range slice {
 		slice[i] = make([][]int, y)
 		for j := range slice[i] {
-			slice[i][j] = make([]int, z)
+			slice[i][j] = make([]int, _z)
 		}
 	}
 
@@ -138,6 +138,62 @@ func TestMoreComplexRegex(t *testing.T) {
 
 	indexesOut[0][0][0] = 0
 	indexesOut[0][0][1] = 5
+
+	assert.ErrorIs(t, err, nil)
+	assert.EqualValues(t, matchingString, &matchingStringOut)
+	assert.EqualValues(t, indexes, &indexesOut)
+}
+
+func TestFewWordsMatched(t *testing.T) {
+	regex := "^a[a-z]+a$"
+	text := []string{"word", "aboba", "abaunda"}
+
+	// Combine the []string into a single string (e.g., with spaces)
+	combinedString := strings.Join(text, "\n")
+
+	// Convert the string to an io.Reader
+	reader := strings.NewReader(combinedString)
+
+	re := regexp.MustCompile(regex)
+
+	matchingString, indexes, err := GetMatchIndexesArray(reader, re)
+
+	matchingStringOut := []string{"aboba", "abaunda"}
+	indexesOut := init3DSlice(2, 1, 2)
+
+	indexesOut[0][0][0] = 0
+	indexesOut[0][0][1] = 5
+
+	indexesOut[1][0][0] = 0
+	indexesOut[1][0][1] = 7
+
+	assert.ErrorIs(t, err, nil)
+	assert.EqualValues(t, matchingString, &matchingStringOut)
+	assert.EqualValues(t, indexes, &indexesOut)
+}
+
+func TestFewMatchesInWord(t *testing.T) {
+	regex := "(ab|ba)"
+	text := []string{"word", "aboba"}
+
+	// Combine the []string into a single string (e.g., with spaces)
+	combinedString := strings.Join(text, "\n")
+
+	// Convert the string to an io.Reader
+	reader := strings.NewReader(combinedString)
+
+	re := regexp.MustCompile(regex)
+
+	matchingString, indexes, err := GetMatchIndexesArray(reader, re)
+
+	matchingStringOut := []string{"aboba"}
+	indexesOut := init3DSlice(1, 2, 2)
+
+	indexesOut[0][0][0] = 0
+	indexesOut[0][0][1] = 2
+
+	indexesOut[0][1][0] = 3
+	indexesOut[0][1][1] = 5
 
 	assert.ErrorIs(t, err, nil)
 	assert.EqualValues(t, matchingString, &matchingStringOut)
